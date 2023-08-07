220bdb585932948f46af034226d2722502da13f0
Lucas Henrique de Souza <55006206+lucasdesouza-dev@users.noreply.github.com>
2023-08-07 09:10:23 -0300

    mehoria projetos

--- src/components/Projetos/index.tsx
+++ src/components/Projetos/index.tsx
@@ -1,19 +1,41 @@
 import React from "react";
 import CardProjetos from "../Cards/CardProjetos";
 import Carousel from "../Carousel";
-
+import { cards } from "@/services/Projetos.service";
 export default function Projetos() {
+
   return (
     <section
       id="projetos"
-      className="bg-gray-200 dark:bg-[#1F2326] flex flex-col items-center justify-center w-full h-full min-h-screen"
+      className="bg-gray-200 dark:bg-[#1F2326] flex flex-col  w-full h-full"
     >
-      <div className="px-8 w-full font-semibold  m-4">
+      <div className="px-2 w-full font-semibold  m-4">
         <h1 className="text-2xl  border-b-2 w-3/5 border-red-600 text-red-600 ">
           Meus Projetos
         </h1>
       </div>
-      <Carousel />
+      <div className="invisible">
+        {/* <Carousel  /> */}
+      </div>
+      <div className="">
+        {cards.map((item, index) => {
+              const { image, urlGit, urlDemo, descricao, titulo, tecnologias } =
+                item;
+              return (
+                <div key={index} className="w-full ">
+                  <CardProjetos
+                    titulo={titulo}
+                    urlGit={urlGit}
+                    urlDemo={urlDemo}
+                    descricao={descricao}
+                    tecnologias={tecnologias}
+                    referencias={item.referencias}
+                    image={image}
+                  />
+                </div>
+              );
+            })}
+      </div>
     </section>
   );
 }
---
+++ src/services/Projetos.service.ts
@@ -0,0 +1,116 @@
+
+import {
+  SiCreatereactapp,
+  SiCss3,
+  SiGraphql,
+  SiHtml5,
+  SiJavascript,
+  SiReact,
+  SiTypescript,
+  SiVite,
+} from "react-icons/si";
+
+import moviesLibGif from "../../public/Movies-Lib.gif";
+import Gtr6 from "../../public/GTR6.gif";
+import SecretWordGif from "../../public/SecretWord.gif";
+import Calculadora from "../../public/calculadora.gif";
+import BarbeariaAluraGif from "../../public/BarbeariaAlura.gif";
+const cards = [
+  {
+    urlGit: "https://github.com/lucasdesouza-dev/The-Movie-App",
+    urlDemo: "https://the-movie-app-git-master-lucashenriquesouza.vercel.app",
+    referencias: {
+      url: "https://developer.themoviedb.org/docs",
+      titulo: "Themoviedb",
+    },
+    descricao:
+      "Aplicação com intuito de mostrar informações atualizadas de filmes , utizando a api da themoviedb para fornecer informações, ",
+    tecnologias: [
+      { titulo: "React.js", icon: SiReact },
+      { titulo: "Vite.js", icon: SiVite },
+      { titulo: "Css3", icon: SiCss3 },
+      { titulo: "Typescript", icon: SiTypescript },
+    ],
+    titulo: "MoviesLib",
+    image: moviesLibGif,
+  },
+  // {
+  //   urlGit: "https://github.com/lucasdesouza-dev/Projeto-ignite-Lab",
+  //   urlDemo:
+  //     "https://projeto-ignite-lab-git-main-lucashenriquesouza.vercel.app",
+  //   descricao:
+  //     "Projeto onde desenvolvo uma plataforma de eventos  junto com ignite lab da rocketseat implementandoas tecnologias abaixo !",
+  //   tecnologias: [
+  //     { titulo: "React.js", icon: <SiReact /> },
+  //     { titulo: "Vite.js", icon: <SiVite /> },
+  //     { titulo: "Css3", icon: <SiCss3 /> },
+  //     { titulo: "Typescript", icon: <SiTypescript /> },
+  //     { titulo: "Graphql", icon: <SiGraphql /> },
+  //   ],
+  //   referencias: {
+  //     url: "https://www.rocketseat.com.br/ignite",
+  //     titulo: "Ignite-Lab Rocketseat",
+  //   },
+  //   titulo: "Ignite-Lab",
+  // },
+  {
+    urlGit: "https://github.com/lucasdesouza-dev/Barbearia-Alura-Lucas",
+    urlDemo: "https://lucasdesouza-dev.github.io/Barbearia-Alura-Lucas/",
+    descricao:
+      "Aplicaçao de uma Barbearia   contruida no curso de Front-End Da Alura",
+    tecnologias: [
+      { titulo: "HTML5", icon: SiHtml5 },
+      { titulo: "Css3", icon: SiCss3 },
+      {
+        titulo: "Javascript", icon: SiJavascript
+      },
+      ,
+    ],
+    titulo: "Barbearia Alura ",
+    image: BarbeariaAluraGif,
+  },
+  {
+    urlGit: "https://github.com/lucasdesouza-dev/secretword",
+    urlDemo: "https://lucasdesouza-dev.github.io/secretword/",
+    descricao:
+      "Projeto desenvolvido no curso React.JS do 0 a Maestria, ministrado pelo Matheus Bathisti , que consiste em adivinhas a palavra escondida com  a dica e limite de erros!",
+    tecnologias: [
+      { titulo: "React.js", icon: SiReact },
+      { titulo: "Create React App", icon: SiCreatereactapp },
+      { titulo: "Css3", icon: SiCss3 },
+    ],
+    titulo: "Secret Word",
+    image: SecretWordGif,
+  },
+  {
+    urlGit: "https://github.com/lucasdesouza-dev/r6_times-lucas",
+    urlDemo: "https://lucasdesouza-dev.github.io/r6_times-lucas/",
+    descricao:
+      "Projeto no intuito de gerar times Com RRMS proximos para equiparar times e jogar com os amigos na ranquiada, deixando as partidas mais divertidas e competitivas",
+    tecnologias: [
+      { titulo: "HTML5", icon: SiHtml5 },
+      { titulo: "Css3", icon: SiCss3 },
+      { titulo: "Javascript.JS", icon: SiJavascript },
+    ],
+    titulo: "GTR6- Gerador de Times Raimbow Six Seage",
+    image: Gtr6,
+  },
+  {
+    urlGit: "https://github.com/lucasdesouza-dev/Calculadora",
+    urlDemo: "https://lucasdesouza-dev.github.io/Calculadora/",
+    descricao:
+      "Calculadora construida com o layout da apple , calculadora simples ams funcional",
+    tecnologias: [
+      { titulo: "React.js", icon: SiReact },
+      { titulo: "Vite.js", icon: SiVite },
+      { titulo: "Css3", icon: SiCss3 },
+      {
+        titulo: "Typescript", icon: SiTypescript
+      },
+      ,
+    ],
+    titulo: "Calculadora Apple React.Js",
+    image: Calculadora,
+  },
+]
+export { cards }
