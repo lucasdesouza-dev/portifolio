9ba3254ea51656365e9f7e2198662352cd2d4954
lucassouza_dev <luca.s.ouza@hotmail.com>
2023-08-07 19:01:04 -0300

    ajustes

--- src/components/Projetos/index.tsx
+++ src/components/Projetos/index.tsx
@@ -7,7 +7,7 @@ export default function Projetos() {
   return (
     <section
       id="projetos"
-      className="bg-gray-200 dark:bg-[#1F2326] flex flex-col  lg:items-center lg:justify-center w-full min-h-screen "
+      className="bg-gray-200 dark:bg-[#1F2326] flex flex-col  w-full "
     >
       <div className="px-2 w-full font-semibold  m-4">
         <h1 className="text-2xl  border-b-2 w-3/5 border-red-600 text-red-600 ">
--- src/components/homeUser/index.tsx
+++ src/components/homeUser/index.tsx
@@ -12,9 +12,9 @@ export default function HomeUser({ name, imageUrl, email }: User) {
   return (
     <section
       id="home"
-      className="   w-full h-full min-h-screen flex flex-col lg:flex-row  justify-evenly"
+      className="   w-full h-full min-h-screen flex flex-col lg:flex-row  justify-evenly max-w-full"
     >
-      <div className=" font-semibold  p-2 flex flex-col  lg:justify-center lg:p-4 lg:w-full">
+      <div className=" font-semibold  p-2 flex flex-col  lg:justify-center lg:p-4 lg:w-3/4">
         <h1 className="my-4 text-xl">Olá, bem vindo ao meu portifólio! </h1>
         <div className=" flex flex-col justify-between ">
           <h2 className="text-xl my-2">
@@ -40,10 +40,10 @@ export default function HomeUser({ name, imageUrl, email }: User) {
           </Button>
         </div>
       </div>
-      {/* <motion.div
+      <motion.div
         animate={{ y: [-50, 0, 50, 0, -50], x: [0,50, 120, 0] }}
         transition={{ ease: "linear", duration: 3, repeat: Infinity }}
-        className="  w-1/2 flex justify-center items-center"
+        className="  w-1/2  justify-center items-center hidden lg:flex"
       >
         <Image
           className=" bg-[#dc2626] rounded-full h-[500px] object-contain border-4 border-white z-10"
@@ -52,10 +52,10 @@ export default function HomeUser({ name, imageUrl, email }: User) {
           height={500}
           alt="Picture of the author"
         />
-      </motion.div> */}
-    <div className="w-full flex items-center justify-center overflow-hidden">
+      </motion.div>
+    <div className="w-full flex items-center justify-center overflow-hidden lg:hidden">
       <Image
-          className=" bg-[#dc2626] rounded-full h-[360px] object-contain border-4 border-white "
+          className=" bg-[#dc2626] rounded-full h-[360px]  object-contain border-4 border-white "
           src={profilePic}
           width={360}
           height={300}
